80048bb00a981ff44e2e7ee1cc77b940fef071be
andrewduong77 <36346764+andrewduong77@users.noreply.github.com>
2020-10-03 08:01:17 -0500

    got ArrayList of String to work with ArrayAdapter for ListView

--- Android App/Bluetooth Hydrometer/app/src/main/java/com/example/bluetoothhydrometer/MainActivity.java
+++ Android App/Bluetooth Hydrometer/app/src/main/java/com/example/bluetoothhydrometer/MainActivity.java
@@ -10,17 +10,28 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.text.DateFormatSymbols;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView listViewTemperature;
-    String[] temperatures = {"36 °F at 1.047","42 °F at 1.049","48 °F at 1.051", "54 °F at 1.053", "60 °F at 1.055"};
+//    String[] temperatures = {"36 °F at 1.047","42 °F at 1.049","48 °F at 1.051","54 °F at 1.053","60 °F at 1.055"};
+    ArrayList<String> temperatures = new ArrayList<String>();
+
+    void getTemperatures() {
+        temperatures.add("36 °F at 1.047");
+        temperatures.add("42 °F at 1.049");
+        temperatures.add("48 °F at 1.051");
+        temperatures.add("54 °F at 1.053");
+        temperatures.add("60 °F at 1.055");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listViewTemperature = findViewById(R.id.listViewTemperature);
+        getTemperatures();
 //        ArrayAdapter<String> temperatureAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, temperatures);
         ArrayAdapter<String> temperatureAdapter = new ArrayAdapter<>(this, R.layout.list_item, temperatures);
         listViewTemperature.setAdapter(temperatureAdapter);
