640b90684f3b250499b82c3b7063a6ea83d25ddf
andrewduong77 <36346764+andrewduong77@users.noreply.github.com>
2020-10-21 18:48:18 -0500

    created BluetoothDemo, refused to build due to symbols not recognized

---
+++ Android App/BluetoothDemo/app/src/main/java/com/example/bluetoothdemo/DeviceInfoModel.java
@@ -0,0 +1,24 @@
+package com.example.bluetoothdemo;
+
+public class DeviceInfoModel {
+
+    private String deviceName, deviceHardwareAddress;
+
+    public DeviceInfoModel(){}
+
+    public DeviceInfoModel(String deviceName, String deviceHardwareAddress){
+        this.deviceName = deviceName;
+        this.deviceHardwareAddress = deviceHardwareAddress;
+    }
+
+    public String getDeviceName(){return deviceName;}
+
+    public String getDeviceHardwareAddress(){return deviceHardwareAddress;}
+
+}
+
+//package com.example.bluetoothdemo;
+//
+//public class DeviceInfoModel {
+//
+//}
